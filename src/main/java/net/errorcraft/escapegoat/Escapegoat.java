bc37f38aa544ddf3084e990ca2391b02eebc3ae8
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2025-02-06 21:08:49 +0100

    Use latest snapshot version instead

---
+++ src/main/java/net/errorcraft/escapegoat/Escapegoat.java
@@ -0,0 +1,9 @@
+package net.errorcraft.escapegoat;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Escapegoat implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
